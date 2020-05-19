3957ba84e97ab00c9dd92f3e9e4dd74b28cf6d90
Anders Bruihler <6530276+ABruihler@users.noreply.github.com>
2020-05-19 16:20:30 -0500

    Add critical_method_change_notify probe

---
+++ critical.py
@@ -0,0 +1,11 @@
+# example file
+
+
+def critical_method():
+    # example method tracked in critical_method_change_notify probe
+    return 1
+
+
+if __name__ == "__main__":
+    critical = critical_method()
+    print("critical_method returned: " + str(critical))
